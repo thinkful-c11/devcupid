3e68d7ab8d8eaafff1448b681ab1e86afe3b581d
Avi <avirzajac@gmail.com>
2017-08-22 10:41:49 -0400

    User Model Schema for Mongo done whee

--- server/models.js
+++ server/models.js
@@ -1,14 +1,46 @@
 // 'use strict';
 const express = require("express");
 const mongoose = require("mongoose");
-const devCupid = mongoose.Schema({
+const userModel = mongoose.Schema({
+    gitHub: {
+        id: {type: String},
+        login: {type: String},
+        avatar_url: {type: String},
+        html_url: {type: String},
+        name: {type: String},
+        company: {type: String},
+        blog: {type: String},
+        location: {type: String},
+        email: {type: String},
+        hireable: {type: Boolean},
+        bio: {type: String}
+    },
+    userInput: {
+        avatar_url: {type: String},
+        name: {type: String},
+        company: {type: String},
+        personal_website: {type: String},
+        location: {type: String},
+        email: {type: String},
+        bio: {type: String},
+        social: {
+            linked_in: {type: String},
+            twitter: {type: String},
+            blog: {type: String}
+        }
+    }
+});
+const techQuestions = mongoose.Schema({
 
 });
-devCupid.methods.apiRepr = function() {
+const personality = mongoose.Schema({
+
+});
+userModel.methods.apiRepr = function() {
     return {
         id: this._id,
 
     };
 };
-const fillerHere = mongoose.model("DevCupid", devCupid);
+const fillerHere = mongoose.model("UserModel", userModel);
 module.exports = { DevCupid };
