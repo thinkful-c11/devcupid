1924ce6fd108f467227a23233b17c481c0a35fc1
tckastanek <tckastanek@gmail.com>
2017-08-22 12:25:38 -0400

    Added a Submit Button.

---
+++ client/components/onboarding/SubmitButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default (props) => {
+  return (
+    <button
+      href='#'
+      className='submit-button'
+      type='submit'
+    >
+      Submit
+    </button>
+  );
+};
