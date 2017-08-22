2b248cb60ab3c6bdc576823967e194189c542949
williamtwobit <william@twobit.cc>
2017-08-22 16:59:50 -0400

    fixed merge conflict on app.js

--- client/components/App.js
+++ client/components/App.js
@@ -4,11 +4,7 @@ import '../SCSS/App.scss';
 export class App extends React.Component {
     render() {
         return (
-<<<<<<< HEAD
             <div className="temporary-button"><a href="/auth/github/callback">CLICK ME TO SIGN IN FOR NOW</a></div>
-=======
-            <div className="test">Hello World</div>
->>>>>>> origin/dev
         );
     }
 }
