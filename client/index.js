091c7bff31a9027379620b208dea5fc23d925c50
tckastanek <tckastanek@gmail.com>
2017-08-22 11:25:34 -0400

    Setup initial front-end folder layout.

--- client/index.js
+++ client/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 // import { Provider } from 'react-redux';
 
 // import store from './store';
-import { App } from './components/App';
+import { App } from './containers/App';
 
 // import './components/index.css';
 
 ReactDOM.render(
     // <Provider store={store}>
-        <App />,
+  <App />,
     // </Provider>,
     document.getElementById('root')
 );
- 
