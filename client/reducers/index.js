3d4e2fd386ff83a2bcb8187e097568acf6e1e65f
tckastanek <tckastanek@gmail.com>
2017-08-22 11:31:37 -0400

    Stubbed out /reducers index.js.

---
+++ client/reducers/index.js
@@ -0,0 +1,15 @@
+import * as actions from '../actions';
+
+const initialState = {
+  loading: false,
+  error: null
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default reducer;
