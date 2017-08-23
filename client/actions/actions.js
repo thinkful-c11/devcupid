7316b36bdbcd2819babc48f735a07bc12de9f632
tckastanek <tckastanek@gmail.com>
2017-08-23 14:12:53 -0400

    Stubbed out update_profile actions.

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -15,3 +15,39 @@ export const textInput_handler = (key, value) => ({
   key,
   value
 });
+
+// Actions for PUT request to update profile.
+export const update_request = () => ({
+  type: ref.UPDATE_REQUEST
+});
+export const update_success = (profile) => ({
+  type: ref.UPDATE_SUCCESS,
+  profile
+});
+export const update_error = (error) => ({
+  type: ref.UPDATE_ERROR,
+  error
+});
+export const update_profile = (githubId, updateBody) => dispatch => {
+  disptach(update_request());
+  //TODO: verify body formatting matches what DB expects
+  const updateObj = {
+    updateBody
+  };
+  const data = {
+    method: 'PUT',
+    body: JSON.stringify(updateObj)
+  };
+
+  fetch(`/api/update-user/${githubId}`, (req, res) => {
+    if (!res.ok) {
+      return Promise.reject(res.statusText);
+    }
+    return res.json();
+  }).then(profile => {
+    console.log(profile);
+    // dispatch(update_success(profile));
+  }).catch(error => {
+    dispatch(update_error(error));
+  });
+};
