52e21d1575120c4cce1610d0fa2e8f6158d710ca
tckastanek <tckastanek@gmail.com>
2017-08-23 16:34:13 -0400

    Added redux to LoginScreen to allow dispatch().

--- client/containers/LoginScreen.js
+++ client/containers/LoginScreen.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default class LoginScreen extends React.Component {
+export class LoginScreen extends React.Component {
+  handleLogin(e) {
+    e.preventDefault();
+    this.props.dispatch(actions.authGithub());
+  }
   render() {
     let questionId = (this.props.questionId) ? this.props.questionId : 0;
     return(
       <div className='login-container'>
-        <div><a href='/api/auth/github'> Sign In With GitHub </a></div>
+        <div><a href='/api/auth/github'>Sign In With GitHub </a></div>
         <div><Link to={`/onboarding/${questionId}`}> Start Onboarding </Link></div>
       </div>
     );
   }
 }
 
+export default connect()(LoginScreen);
 
-<div className="temporary-button"><a href="/auth/github/callback">CLICK ME TO SIGN IN FOR NOW</a></div>
+
+{/* <div className="temporary-button"><a href="/auth/github/callback">CLICK ME TO SIGN IN FOR NOW</a></div> */}
