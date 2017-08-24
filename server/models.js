8d050bee2e8e170bbf1acf35c2710ef9ee5100a1
tckastanek <tckastanek@gmail.com>
2017-08-24 12:07:04 -0400

    Lint

--- server/models.js
+++ server/models.js
@@ -1,45 +1,45 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const UserModel = mongoose.Schema({
-    onboarded: {type: String},
-    gitHub: {
-        id: {type: String},
-        login: {type: String},
-        avatar_url: {type: String},
-        html_url: {type: String},
-        name: {type: String},
-        company: {type: String},
-        blog: {type: String},
-        location: {type: String},
-        email: {type: String},
-        hireable: {type: Boolean},
-        bio: {type: String}
+  onboarded: {type: String},
+  gitHub: {
+    id: {type: String},
+    login: {type: String},
+    avatar_url: {type: String},
+    html_url: {type: String},
+    name: {type: String},
+    company: {type: String},
+    blog: {type: String},
+    location: {type: String},
+    email: {type: String},
+    hireable: {type: Boolean},
+    bio: {type: String}
+  },
+  profile: {
+    personalTitle: {type: String},
+    passions: {type: Array},
+    skills: {
+      roles: {type: Array},
+      languages: {type: Array},
+      libraries: {type: Array},
+      speciality: {type: Array},
+      softwareTools: {type: Array}
     },
-    profile: {
-        personalTitle: {type: String},
-        passions: {type: Array},
-        skills: {
-            roles: {type: Array},
-            languages: {type: Array},
-            libraries: {type: Array},
-            speciality: {type: Array},
-            softwareTools: {type: Array}
-        },
-        remoteOk: {type: Boolean},
-        avatar_url: {type: String},
-        name: {type: String},
-        company: {type: String},
-        personal_website: {type: String},
-        location: {type: String},
-        email: {type: String},
-        bio: {type: String},
-        social: {
-            linked_in: {type: String},
-            twitter: {type: String},
-            blog: {type: String}
-        }
-    },
-    personality: {  
+    remoteOk: {type: Boolean},
+    avatar_url: {type: String},
+    name: {type: String},
+    company: {type: String},
+    personal_website: {type: String},
+    location: {type: String},
+    email: {type: String},
+    bio: {type: String},
+    social: {
+      linked_in: {type: String},
+      twitter: {type: String},
+      blog: {type: String}
     }
+  },
+  personality: {
+  }
 });
 
 // UserModel.methods.apiRepr = function() {
@@ -49,4 +49,4 @@ const UserModel = mongoose.Schema({
 //     };
 // };
 
-module.exports = mongoose.model("Users", UserModel);
+module.exports = mongoose.model('Users', UserModel);
