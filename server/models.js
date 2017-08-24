cd947d915e59157895d7429084ae65fce8649d36
Avi <avirzajac@gmail.com>
2017-08-24 13:57:08 -0400

    Updated with active for each language in schema

--- server/models.js
+++ server/models.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const LanguageSchema = mongoose.Schema({
     javascript: {
+        _active: {type:Boolean},
         React: {type:Boolean},
         Redux: {type:Boolean},
         Angular: {type:Boolean},
@@ -10,10 +11,12 @@ const LanguageSchema = mongoose.Schema({
         Vue: {type:Boolean},
         "Node.js": {type:Boolean},
     },
-    HTML5:{
+    HTML5: {
+        _active: {type:Boolean},
         Pug: {type:Boolean}
     },
-    CSS3:{
+    CSS3: {
+        _active: {type:Boolean},
         SASS: {type:Boolean},
         LESS: {type:Boolean},
         Bootstrap: {type:Boolean},
@@ -23,39 +26,44 @@ const LanguageSchema = mongoose.Schema({
         "Responsive Design": {type:Boolean},
         "Mobile First": {type:Boolean}
     },
-    C: {type:Boolean},
-    "C++": {type:Boolean},
-    "C#": {type:Boolean},
-    Java:{
+    C: {_active: {type:Boolean}},
+    "C++": {_active: {type:Boolean}},
+    "C#": {_active: {type:Boolean}},
+    Java: {
+        _active: {type:Boolean},
         Swing: {type:Boolean},
         "Spring Boot": {type:Boolean},
         Guava: {type:Boolean}
     },
     PHP: {
+        _active: {type:Boolean},
         Laravel: {type:Boolean},
         Dispatch: {type:Boolean}
     },
     Python: {
+        _active: {type:Boolean},
         Django: {type:Boolean},
         Flask: {type:Boolean}
     },
-    Perl: {type:Boolean},
+    Perl: {_active: {type:Boolean}},
     Ruby: {
+        _active: {type:Boolean},
         Rails: {type:Boolean},
         Sinatra: {type:Boolean}
     },
-    ".NET": {type:Boolean},
-    Go: {type:Boolean},
-    Rust: {type:Boolean},
-    Scala: {type:Boolean},
+    ".NET": {_active: {type:Boolean}},
+    Go: {_active: {type:Boolean}},
+    Rust: {_active: {type:Boolean}},
+    Scala: {_active: {type:Boolean}},
     Clojure: {
+        _active: {type:Boolean},
         Leiningen: {type:Boolean},
         Ring: {type:Boolean},
         Om: {type:Boolean}
     },
-    "Swift/Objective-C": {type:Boolean},
-    Elm: {type:Boolean},
-    "F#": {type:Boolean}
+    "Swift/Objective-C": {_active: {type:Boolean}},
+    Elm: {_active: {type:Boolean}},
+    "F#": {_active: {type:Boolean}},
 });
 const UserModel = mongoose.Schema({
     onboarded: {type: String},
