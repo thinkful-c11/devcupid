63e051a4858cf05e8c68ff90266fd0f051491517
tckastanek <tckastanek@gmail.com>
2017-08-28 11:37:07 -0400

    Setup softwareTools comp.

---
+++ client/components/profile/profileBody/softwareTools.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class SoftwareTools extends React.Component {
+  formatTools(user) {
+    let tools = user.skills.softwareTools;
+    let formatted = [];
+    for (let tool in tools) {
+      if (tools[tool]) {
+        formatted.push(tool);
+      }
+    }
+    return tool;
+  }
+
+  return() {
+    const { user } = this.props;
+    const formattedTools = this.formatTools(user);
+    const tools = formattedTools.map((tool, index) => {
+      return <li key={`passion ${index}`}>{tool}</li>;
+    });
+    return (
+      <ul>
+        {tools}
+      </ul>
+    );
+  }
+}
