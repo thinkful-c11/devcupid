8ebfbdfbb6314c126fc58a83e41ffa07249450c0
tckastanek <tckastanek@gmail.com>
2017-08-29 10:59:50 -0400

    Setup tests for serving the built client (html).

--- server/tests/test-server.js
+++ server/tests/test-server.js
@@ -6,13 +6,13 @@ const chaiHttp = require('chai-http');
 // Backend requirements and files
 const mongoose = require('mongoose');
 const { DATABASE_URL } = require('../../config/config');
-const { app, runServer } = require('../server');
+const { app, runServer, closeServer } = require('../server');
 const { Users, Languages } = require('../models');
 
 // HTTP integration for testing with chai
 chai.use(chaiHttp);
 
-describe('Blog Posts', function() {
+describe('The Built Client', function() {
     // Run the server before we test
   before(function() {
     return runServer();
@@ -22,4 +22,30 @@ describe('Blog Posts', function() {
     return closeServer();
   });
 
+  it('should serve index.html on GET to root', function() {
+    return chai.request(app)
+    .get('/')
+    .then(function(res) {
+      res.should.have.status(200);
+      res.should.be.html;
+    });
+  });
+
+  it('should serve index.html on GET to any non /api/ endpoint', function() {
+    return chai.request(app)
+    .get('/thisisnotavalidendpoint')
+    .then(function(res) {
+      res.should.have.status(200);
+      res.should.be.html;
+    });
+  });
+
+  it('should serve index.html on GET to not explicitly defined /api endpoints', function() {
+    return chai.request(app)
+    .get('/api/thisisabunchofgibberish')
+    .then(function(res) {
+      res.should.have.status(200);
+      res.should.be.html;
+    });
+  });
 });
