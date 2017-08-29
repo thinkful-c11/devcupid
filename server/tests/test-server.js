261084ac266d32115cf4d0a21bb32f87c3c919fa
tckastanek <tckastanek@gmail.com>
2017-08-29 10:12:50 -0400

    Lint and refactor.

--- server/tests/test-server.js
+++ server/tests/test-server.js
@@ -1,27 +1,26 @@
 // Chai
-const chai=require('chai');
-const should=require('chai').should();
-const chaiHttp=require('chai-http');
+const chai = require('chai');
+const should = require('chai').should();
+const chaiHttp = require('chai-http');
 
 // Backend requirements and files
-const mongoose=require('mongoose');
-const {DATABASE_URL}=require('../../config/config');
-const {app,runServer}=require('../server');
-const {Users,Languages}=require('../models');
+const mongoose = require('mongoose');
+const { DATABASE_URL } = require('../../config/config');
+const { app, runServer } = require('../server');
+const { Users, Languages } = require('../models');
 
 // HTTP integration for testing with chai
 chai.use(chaiHttp);
 
-describe('Blog Posts', function() {
+describe('Blog Posts', () => {
 
     // Run the server before we test
-    before(function() {
-        return runServer();
-    });
+  before(() => {
+    return runServer();
+  });
 
     // Close the server once we finish testing
-    after(function() {
-        return closeServer();
-    });
-
+  after(() => {
+    return closeServer();
+  });
 });
