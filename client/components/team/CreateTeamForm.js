3b8255a4884bd53026eecf3b2c96957a149294ca
tckastanek <tckastanek@gmail.com>
2017-09-05 10:21:59 -0400

    Stubbed out a create team form component.

---
+++ client/components/team/CreateTeamForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class CreateTeamForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className='create-team-container'>
+        THIS IS THE CREATE TEAM PAGE
+      </div>
+    );
+  }
+}
