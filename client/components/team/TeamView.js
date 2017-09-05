47c2e600be2b0ba5485c5b4c2baf950c9d15e1cd
tckastanek <tckastanek@gmail.com>
2017-09-05 15:39:55 -0400

    TeamView is wired up to show info and users in an ugly way.

--- client/components/team/TeamView.js
+++ client/components/team/TeamView.js
@@ -4,8 +4,8 @@ export default class TeamView extends React.Component {
   render() {
     console.log('VIEW', this.props);
     const {
+      // Team Members, NOTE: admins may dupe with the rest
       admins,
-      // Team Members
       designers,
       developers,
       founders,
@@ -19,6 +19,40 @@ export default class TeamView extends React.Component {
       email
     } = this.props.activeTeam;
 
+    // This will need to be pulled out at some point
+    // in order to make DB requests for all the info.
+    const teamList = (activeTeam) => {
+      const isReady = Object.getOwnPropertyNames(activeTeam).length;
+      if (isReady) {
+        // Creating a Set to eliminate duplicate roles.
+        const membersArray = [
+          ...admins,
+          ...designers,
+          ...developers,
+          ...founders,
+          ...projectManagers
+        ];
+        const membersSet = new Set([...membersArray]);
+
+        const members = [...membersSet];
+
+        const membersList = members.map((m, index) => {
+          return (
+            <li key={`${m}-${index}`}>
+              {m}
+            </li>
+          );
+        });
+
+        return (
+          <ul className='members-list'>
+            {membersList}
+          </ul>
+        );
+      }
+      else return 'LOADING';
+    };
+
     return (
       <div className='team-view-container'>
         <div className='team-info-conatainer'>
@@ -26,6 +60,10 @@ export default class TeamView extends React.Component {
           <h1>{name}</h1>
           <h3>{description}</h3>
           <h4>{location}</h4>
+        </div>
+
+        <div className='team-members-container'>
+          {teamList(this.props.activeTeam)}
         </div>
       </div>
     );
