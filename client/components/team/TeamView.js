d76d7b86d88acdf11adde8cce4ebd3d04472bdeb
tckastanek <tckastanek@gmail.com>
2017-09-05 15:07:09 -0400

    Initial setup of TeamView.

--- client/components/team/TeamView.js
+++ client/components/team/TeamView.js
@@ -2,9 +2,31 @@ import React from 'react';
 
 export default class TeamView extends React.Component {
   render() {
+    console.log('VIEW', this.props);
+    const {
+      admins,
+      // Team Members
+      designers,
+      developers,
+      founders,
+      projectManagers,
+
+      // Team Info
+      avatar_url,
+      name,
+      description,
+      location,
+      email
+    } = this.props.activeTeam;
+
     return (
       <div className='team-view-container'>
-        THIS IS THE TEAM VIEW
+        <div className='team-info-conatainer'>
+          <img src={avatar_url} alt='Team Image' />
+          <h1>{name}</h1>
+          <h3>{description}</h3>
+          <h4>{location}</h4>
+        </div>
       </div>
     );
   }
