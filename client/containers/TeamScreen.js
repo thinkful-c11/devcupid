026a68f2b8ed9988e0dd77acf175df72c2547133
tckastanek <tckastanek@gmail.com>
2017-09-05 10:33:52 -0400

    Stubbed out a fetch_team action based on URL params.

--- client/containers/TeamScreen.js
+++ client/containers/TeamScreen.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../../actions/actions';
+import * as Cookies from 'js-cookie';
 
 import CreateTeamForm from '../team/CreateTeamForm';
 
 export class TeamScreen extends React.Component {
+  componentDidMount() {
+    const teamId = this.props.match.params.teamId;
+    const accessToken = Cookies.get('accessToken');
+
+    // TODO: add a team to store and pull in as a prop
+    if (teamId !== 'create') {
+      this.props.dispatch(actions.fetch_team(accessToken, teamId));
+    }
+  }
+  
   render () {
     const route = this.props.match.params.teamId;
     const view = (route) => {
