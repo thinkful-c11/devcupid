d92d5537682f0366bcd031f4b498cd763d3ac93f
tckastanek <tckastanek@gmail.com>
2017-09-07 10:27:52 -0400

    Removed firebaseConfig.js

--- config/firebaseConfig.js
+++
@@ -1,6 +0,0 @@
-export const FIREBASE_API_KEY='AIzaSyAkoL7gY5AMqCwKDhJ_RF3hNnBcQZv4kfg';
-export const FIREBASE_AUTH_DOMAIN='devcupid-400c8.firebaseapp.com';
-export const FIREBASE_DATABASE_URL='https://devcupid-400c8.firebaseio.com';
-export const FIREBASE_PROJECT_ID='devcupid-400c8';
-export const FIREBASE_STORAGE_BUCKET='devcupid-400c8.appspot.com';
-export const FIREBASE_MESSAGING_SENDER_ID='287438957512';
