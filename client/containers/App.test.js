03768eaca798b34e996616f0e3604905458d256a
Avi <avirzajac@gmail.com>
2017-09-07 18:02:16 -0400

    Removed the second App.test because the main tests folder works now yay

--- client/containers/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import {App} from '../../containers/App.js';
-describe('<App />',()=>{
-    it('Render without crashing',()=>{
-        shallow(<App />);
-    });
-});
