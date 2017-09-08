75b17bfeee0e00213c376a5c7786f6474bb05720
Avi <avirzajac@gmail.com>
2017-09-08 03:03:33 -0400

    Removed console.log

--- client/components/search/SearchItem.js
+++ client/components/search/SearchItem.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 export default class SearchItem extends React.Component {
     render() {
-        console.log(this.props.user);
         return (
             <div className="search-item">
                 <p>Name: {this.props.user.profile.name}</p>
