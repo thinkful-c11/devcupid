7abaf76d97ce01a50359b11f28cdfe3f83df7127
tckastanek <tckastanek@gmail.com>
2017-09-08 10:18:01 -0400

    Added env variables to fire.js

--- client/fire.js
+++ client/fire.js
@@ -8,12 +8,12 @@ import {
   FIREBASE_MESSAGING_SENDER_ID } from '../config/firebaseConfig';
 
 const config = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  databaseURL: FIREBASE_DATABASE_URL,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID
+  apiKey: process.env.FIREBASE_API_KEY || FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN || FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL || FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID || FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || FIREBASE_MESSAGING_SENDER_ID
 };
 
 const fire = firebase.initializeApp(config);
