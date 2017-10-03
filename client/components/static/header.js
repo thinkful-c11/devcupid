372343c8781b5ce6c18b2cab59f0c929b7e4b0e3
wilm <william@twobit.cc>
2017-10-03 10:05:28 -0400

    added mobile state explainer and tweaked menu

--- client/components/static/header.js
+++ client/components/static/header.js
@@ -1,73 +1,87 @@
-import React from 'react';
-import '../../SCSS/header.scss';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from "react";
+import "../../SCSS/header.scss";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-export class Header extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      navOpen:false
-    }
-  }
+export class Header extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			navOpen: false,
+		};
+	}
 
-  handleNav(){
-    let navOpen = !this.state.navOpen
-    this.setState({
-      navOpen: navOpen
-    });
-  }
+	handleNav() {
+		let navOpen = !this.state.navOpen;
+		this.setState({
+			navOpen: navOpen,
+		});
+	}
 
-  render(){
-    let header;
-    console.log(this.props.user);
-    if(this.props.loggedIn){
-      header = (
-        <div className='topBar content container'>
-          <div className='logo'><img src='/public/logo.svg' /></div>
-          <nav>
-            <img src={this.props.user.avatar_url} className="navThumbnail" onClick={ ()=> this.handleNav() }/>
-            <div className='menu'>menu</div>
-            <ul className={this.state.navOpen ? "userNav" : "userNav closed"}>
-              <li>
-                <Link to={'/search'} onClick={ ()=> this.handleNav() }> Search </Link>
-              </li>
-              <li>
-                <Link to='/team/myteams' onClick={ ()=> this.handleNav() }>Your Teams</Link>
-              </li>
-              <li>
-                <Link to={'/me'} onClick={ ()=> this.handleNav() }> View Your Profile </Link>
-              </li>
-              <li>
-                <a href='/api/auth/github/logout' onClick={ ()=> this.handleNav() }>Signout</a>
-              </li>
-            </ul>
-          </nav>
-          {/* <div className='burgerMenu'>
-            <div className='burger top part' />
-            <div className='burger middle part' />
-            <div className='burger bottom part' />
-          </div> */}
-        </div>
-      );
-    }
-    else{
-      header = (
-        <div className='topBar content container landing'>
-          <div className='logo'><img src='/public/logo.svg' /></div>
-        </div>
-      );
-    }
-    return(
-      <header className='container'>
-        {header}
-      </header>
-    );
-  }
+	render() {
+		let header;
+		console.log(this.props.user);
+		if (this.props.loggedIn) {
+			header = (
+				<div className="topBar content container">
+					<div className="logo">
+						<Link to="/">
+							<img src="/public/logo.svg" />
+						</Link>
+					</div>
+					<nav>
+						<div className="thumbnail" onClick={() => this.handleNav()}>
+							<img
+								src={this.props.user.avatar_url}
+								className="navThumbnail"
+								onClick={() => this.handleNav()}
+							/>
+							<div className="menu">menu</div>
+						</div>
+						<ul className={this.state.navOpen ? "userNav" : "userNav closed"}>
+							<li>
+								<Link to={"/search"} onClick={() => this.handleNav()}>
+									{" "}
+									Search{" "}
+								</Link>
+							</li>
+							<li>
+								<Link to="/team/myteams" onClick={() => this.handleNav()}>
+									Your Teams
+								</Link>
+							</li>
+							<li>
+								<Link to={"/me"} onClick={() => this.handleNav()}>
+									{" "}
+									View Your Profile{" "}
+								</Link>
+							</li>
+							<li>
+								<a
+									href="/api/auth/github/logout"
+									onClick={() => this.handleNav()}>
+									Signout
+								</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			);
+		} else {
+			header = (
+				<div className="topBar content container landing">
+					<div className="logo">
+						<img src="/public/logo.svg" />
+					</div>
+				</div>
+			);
+		}
+		return <header className="container">{header}</header>;
+	}
 }
 
 const mapStateToProps = state => ({
-  user: state.profile
-})
+	user: state.profile,
+});
 
 export default connect(mapStateToProps)(Header);
